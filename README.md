7d57dd18f90321e03a1ac53c3cae1e182d5eb40c
Tucker Guen <tkg11@case.edu>
2020-10-16 15:51:57 -0400

    Add performance report to readme

--- README.md
+++ README.md
@@ -1,13 +1,11 @@
 ## Running the Simulation and Node for Testing pfc_init Performance
-### Configuring the Workspace
-Option 1:
-1. Clone the entire catkin workspace src directory into a new catkin workspace (find at: https://github.com/tuckerguen/pfc_init_catkin_ws).
-2. Build the workspace, resolve errors
-Option 2:
-1. Clone the pfc_init project into an empty catkin workspace src directory
+### Configuring the Workspace  
+1. Clone the pfc_init project into a catkin workspace src directory
 2. Resolve the missing dependencies manually
+    * Ros package dependencies can be found in the full workspace [here](fhttps://github.com/tuckerguen/pfc_init_catkin_ws) 
+    * *Note: Do not clone and build directly from the linked workspace above*
 ### Running the Simulation
-3. catkin build the project, resolve errors
+3. "catkin build" the project, resolve errors
 4. source devel/setup.bash from the root dir of the catkin workspace
 5. In terminal run: roslaunch sim_gazebo test_pfc_init.launch (there may be errors but just check that the needle and background are spawned properly)
     * You can confirm that the simulation is running properly using (in a new terminal): "rosrun image_view image_view image:=/davinci_endo/left/image_raw"
@@ -17,4 +15,78 @@ Option 2:
 7. rosrun pfc_init pfc_init_node
 8. You may see various images during the process (these are mostly used for debugging). 
     * To continue, press any key
-9. Once the program is done running you'll see an output of the results in the terminal as well as in image form
+9. Once the program is done running you'll see an output of the results in the terminal and a new csv file containing 
+the results will appear in the test_results directory, with the name being the time that the program was run.
+
+# Current (as of 10/16/20) Performance Metrics
+## General Performance
+Run with parameters:
+
+| Parameter                                                	| Value 	|
+|----------------------------------------------------------	|----------	|
+| z range                                                  	| 0.7-0.18 	|
+| z increment 	                                            | 0.01 	    |
+| Rotation range (all axes)                                 | 0-360 	| 
+| Rotation increment (all axes)	                            | 30	    |
+| # pts in needle template                                 	| 10       	|
+| Number of random poses                           	        | 50      	|
+| Candidate choice for each pose                            | Min error for metric |
+
+These parameters for these were picked based on the [Incrementing Metrics](#Incrementing Metrics) data, choosing the values
+that maximized performance while minimizing runtime.  
+  
+For each individual axis rotation, there is no rotation in the other 2 axes.
+### Results
+
+| Axis                                                	| Location Error (mm) | Rotation Error (deg) | Execution Time (s) |
+|-------------------------------------------------------|----------      |-----           |----            | 
+| Yaw                                                   | 1.6711       |   4.6892    |  1.357             | 
+| Pitch                                                 | 2.9311       |   11.2032   |  1.6305        | 
+| Roll                                                  | 2.9620       |   5.2490    |  1.4833      | 
+| All                                                   | 4.8257       |   69.3441   |  13.5491     |
+
+
+
+## Incrementing Metrics
+There are four parameters that have the greatest effect on performance when running the particle filter auto-initialization:  
+* Yaw, Pitch, and Roll increments  (from min to max value, how much to increment for each template matching iteration)  
+* Number of candidate points to return (Top n candidate points based on template matching score)  
+
+For each of these, we are concerned with their effects on runtime, location error, and rotation error.  
+  
+The following tests were conducted with fixed parameters of:  
+
+| Parameter                                                	| Value 	|
+|----------------------------------------------------------	|----------	|
+| Rotation d.o.f                                            | 1	(rotating only in axis being measured)|
+| z range                                                  	| 0.7-0.18 	|
+| z increment 	                                            | 0.01 	    | 
+| # pts in needle template                                 	| 10       	|
+| # random poses                           	        | 200      	|
+| # Candidate points (if not measured metric)   | 10
+| Candidate choice for each pose                            | Min error for metric          |
+
+*A note about execution time*: The execution time indicated here is for rotating on a single axis at a time and 
+is therefore not indicative of overall performance. When rotating on all three axes, the number of iterations, and 
+in turn the execution time is multiplicative (except in the case of candidate point generation which occurs once per run)
+The plots are just there to indicate the effect of each on execution time in general.
+
+### Yaw Increment
+![Yaw increment vs Location error](imgs/yaw_inc_vs_time1.jpeg)  
+![Yaw increment vs Location error](imgs/yaw_inc_vs_loc_err1.jpeg)  
+![Yaw increment vs Location error](imgs/yaw_inc_vs_rot_err1.jpeg)  
+
+### Pitch Increment
+![pitch increment vs Location error](imgs/pitch_inc_vs_time1.jpeg)  
+![pitch increment vs Location error](imgs/pitch_inc_vs_loc_err1.jpeg)  
+![pitch increment vs Location error](imgs/pitch_inc_vs_rot_err1.jpeg)  
+
+### Roll Increment
+![roll increment vs Location error](imgs/roll_inc_vs_time1.jpeg)  
+![roll increment vs Location error](imgs/roll_inc_vs_loc_err1.jpeg)  
+![roll increment vs Location error](imgs/roll_inc_vs_rot_err1.jpeg)  
+
+### Candidate Point Metrics
+![num_cand_pts increment vs Location error](imgs/num_cand_pts_vs_time1.jpeg)  
+![num_cand_pts increment vs Location error](imgs/num_cand_pts_vs_loc_err1.jpeg)  
+![num_cand_pts increment vs Location error](imgs/num_cand_pts_vs_rot_err1.jpeg)  
--- src/pfc_init_node.cpp
+++ src/pfc_init_node.cpp
@@ -13,78 +13,70 @@
 #include <chrono>
 #include <ctime>
 #include <sstream>
-#include <cstdlib>
 #include <random>
 #include <fstream>
-
-void writeDataListToCSV(vector<vector<string>> dataList)
-{
-    ofstream data_file;
-    auto now = std::chrono::system_clock::now();
-    auto in_time_t = std::chrono::system_clock::to_time_t(now);
-
-    std::stringstream ss;
-    ss << std::put_time(std::localtime(&in_time_t), "%Y-%m-%d-%X");
-    string time = ss.str();
-    data_file.open("src/pfc_init/test_results/pfcinit_performance_data_" + time + ".csv");
-
-    if (data_file.fail()){
-        cout << "couldn't open file" << endl;
-    }
-
-    for(int i = 0; i < dataList.size(); i++){
-        for(int j = 0; j < dataList.at(i).size(); j++)
-        {
-            string data_point = dataList.at(i).at(j);
-            if(j < dataList.at(i).size()-1)
-                data_file << data_point << ", ";
-            else
-                data_file << data_point;
-        }
-        data_file << "\n";
-    }
-}
+#include <ros/package.h>
 
 using namespace std;
 
-cv::Mat l_img;
-cv::Mat r_img;
-void CB_cam_l(const sensor_msgs::ImageConstPtr& i){
-	try{
-		cv::Mat src =  cv_bridge::toCvShare(i, sensor_msgs::image_encodings::BGR8)->image;
-		l_img = src.clone();
-	} catch(cv_bridge::Exception& e){
-		ROS_ERROR("Could not convert from '%s' to 'bgr8'.", i ->encoding.c_str());
+void writeDataListToCSV(vector<vector<string>> dataList, string time_str) {
+	ofstream data_file;
+
+	std::string path = ros::package::getPath("pfc_init");
+	data_file.open(path + "/test_results/pfcinit_performance_data_" + time_str + ".csv");
+
+	if (data_file.fail()) {
+		cout << "couldn't open file" << endl;
 	}
-}
-void CB_cam_r(const sensor_msgs::ImageConstPtr& i){
-	try{
-		cv::Mat src =  cv_bridge::toCvShare(i, sensor_msgs::image_encodings::BGR8)->image;
-		r_img = src.clone();
-	} catch(cv_bridge::Exception& e){
-		ROS_ERROR("Could not convert from '%s' to 'bgr8'.", i ->encoding.c_str());
+
+	for (int i = 0; i < dataList.size(); i++) {
+		for (int j = 0; j < dataList.at(i).size(); j++) {
+			string data_point = dataList.at(i).at(j);
+			if (j < dataList.at(i).size() - 1)
+				data_file << data_point << ", ";
+			else
+				data_file << data_point;
+		}
+		data_file << "\n";
 	}
 }
 
-float rng(float min, float max)
-{
-    //https://en.cppreference.com/w/cpp/numeric/random/uniform_int_distribution
-    std::random_device rd;
-    std::mt19937 gen(rd());
+cv::Mat l_img;
+cv::Mat r_img;
+void CB_cam_l(const sensor_msgs::ImageConstPtr &i) {
+	try {
+		cv::Mat src = cv_bridge::toCvShare(i, sensor_msgs::image_encodings::BGR8)->image;
+		l_img = src.clone();
+	} catch (cv_bridge::Exception &e) {
+		ROS_ERROR("Could not convert from '%s' to 'bgr8'.", i->encoding.c_str());
+	}
+}
+void CB_cam_r(const sensor_msgs::ImageConstPtr &i) {
+	try {
+		cv::Mat src = cv_bridge::toCvShare(i, sensor_msgs::image_encodings::BGR8)->image;
+		r_img = src.clone();
+	} catch (cv_bridge::Exception &e) {
+		ROS_ERROR("Could not convert from '%s' to 'bgr8'.", i->encoding.c_str());
+	}
+}
+
+float rng(float min, float max) {
+	//https://en.cppreference.com/w/cpp/numeric/random/uniform_int_distribution
+	std::random_device rd;
+	std::mt19937 gen(rd());
 	std::uniform_real_distribution<double> dist(min, max);
 	return dist(gen);
 }
 
-int main(int argc, char** argv)
-{
-    ros::init(argc, argv, "pfc_init_node");
+int main(int argc, char **argv) {
+	ros::init(argc, argv, "pfc_init_node");
 
 	ros::NodeHandle nh;
 	ros::Subscriber l_cam_sub;
 	ros::Subscriber r_cam_sub;
 
 	/** Set up subscribers for images */
-	l_cam_sub = nh.subscribe("/davinci_endo/left/image_raw" , 1, &CB_cam_l);
+	l_cam_sub = nh.subscribe("/davinci_endo/left/image_raw", 1, &CB_cam_l);
 	r_cam_sub = nh.subscribe("/davinci_endo/right/image_raw", 1, &CB_cam_r);
 
 	/** Get camera intrinsics */
@@ -94,143 +86,160 @@ int main(int argc, char** argv)
 	sensor_msgs::CameraInfo r_inf = *(
 		ros::topic::waitForMessage<sensor_msgs::CameraInfo>("/davinci_endo/right/camera_info", nh)
 	);
-	cv::Mat P_l = cv::Mat(3, 4, CV_64FC1, (void *) l_inf.P.data());
-    cv::Mat P_r = cv::Mat(3, 4, CV_64FC1, (void *) r_inf.P.data());
+	cv::Mat P_l = cv::Mat(3, 4, CV_64FC1, (void *)l_inf.P.data());
+	cv::Mat P_r = cv::Mat(3, 4, CV_64FC1, (void *)r_inf.P.data());
 
-	// Configure csv key (first row in the csv)
-	int max_cand_pts = 35;
+	/* Configure csv key (first row in the csv) */
+	int max_cand_pts = 30;
 	vector<string> csv_key_base
-	{ 
-		"time", "loc_err", "rot_err"
-		//, "est_loc_x", "est_loc_y", "est_loc_z", "est_rot_x", "est_rot_y", "est_rot_z", "est_rot_w",
-	};
+		{
+			"time", "loc_err", "rot_err"
+			//, "est_loc_x", "est_loc_y", "est_loc_z", "est_rot_x", "est_rot_y", "est_rot_z", "est_rot_w",
+		};
 	vector<string> csv_key
-	{
-		"true_loc_x", "true_loc_y", "true_loc_z", "true_rot_x", "true_rot_y", "true_rot_z", "true_rot_w",
-		"yaw_min", "yaw_max", "yaw_inc",
-		"roll_min", "roll_max", "roll_inc",
-		"pitch_min", "pitch_max", "pitch_inc",
-		"z_min", "z_max", "z_inc", "num_cand_pts"
-	};
-	for(int i = 0; i < max_cand_pts; i++)
-	{
+		{
+			"true_loc_x", "true_loc_y", "true_loc_z", "true_rot_x", "true_rot_y", "true_rot_z", "true_rot_w",
+			"yaw_min", "yaw_max", "yaw_inc",
+			"roll_min", "roll_max", "roll_inc",
+			"pitch_min", "pitch_max", "pitch_inc",
+			"z_min", "z_max", "z_inc", "num_cand_pts"
+		};
+	for (int i = 0; i < max_cand_pts; i++) {
 		csv_key.insert(csv_key.end(), csv_key_base.begin(), csv_key_base.end());
 	}
-
-
 	vector<vector<string>> all_results;
 	all_results.insert(all_results.begin(), csv_key);
 
-	// Get current time as string for naming test file
+	/* Get current time as string for naming test file */
 	auto now = std::chrono::system_clock::now();
 	auto in_time_t = std::chrono::system_clock::to_time_t(now);
 	std::stringstream ss;
 	ss << std::put_time(std::localtime(&in_time_t), "%Y-%m-%d-%X");
 	string time = ss.str();
 
-	// Random needle position boundaries
+	/* Needle position boundaries */
 	float x_min = -0.045, x_max = 0.045;
 	float y_min = -0.035, y_max = 0.02;
 	float z_min = 0.07, z_max = 0.18;
 
-	// Compute random set of poses
+	/* Generate set of random poses */
 	int num_poses = 50;
 	vector<geometry_msgs::Pose> poses;
-
-	for(int i = 0; i < num_poses; i++){
+	for (int i = 0; i < num_poses; i++) {
 		geometry_msgs::Pose test_pose;
 		test_pose.position.x = rng(x_min, x_max);
 		test_pose.position.y = rng(y_min, y_max);
 		test_pose.position.z = rng(z_min, z_max);
 
 		//http://planning.cs.uiuc.edu/node198.html
-		float u = rng(0,1), v = rng(0,1), w = rng(0,1);
-		test_pose.orientation.y = sqrt(1-u) * cos(2*M_PI*v);
-		test_pose.orientation.z = sqrt(u) * sin(2*M_PI*w);
-		test_pose.orientation.w = sqrt(u) * cos(2*M_PI*w);
-		test_pose.orientation.x = sqrt(1-u) * sin(2*M_PI*v);
-		
+//		float u = rng(0, 1), v = rng(0, 1), w = rng(0, 1);
+//		test_pose.orientation.y = sqrt(1 - u)*cos(2*M_PI*v);
+//		test_pose.orientation.z = sqrt(u)*sin(2*M_PI*w);
+//		test_pose.orientation.w = sqrt(u)*cos(2*M_PI*w);
+//		test_pose.orientation.x = sqrt(1 - u)*sin(2*M_PI*v);
+
 		// For testing only one rotation
-//		 double radians = pfc::deg2rad * rng(0,360);
-//		 Eigen::Quaternionf q;
-//		 q = Eigen::AngleAxisf(0, Eigen::Vector3f::UnitX()) //roll
-//		 	* Eigen::AngleAxisf(0, Eigen::Vector3f::UnitY()) //pitch
-//		 	* Eigen::AngleAxisf(radians, Eigen::Vector3f::UnitZ()); //yaw
-//		 test_pose.orientation.x = q.x();
-//		 test_pose.orientation.y = q.y();
-//		 test_pose.orientation.z = q.z();
-//		 test_pose.orientation.w = q.w();
+		 double radians = pfc::deg2rad * rng(0,360);
+		 Eigen::Quaternionf q;
+		 q = Eigen::AngleAxisf(0, Eigen::Vector3f::UnitX()) //roll
+		 	* Eigen::AngleAxisf(radians, Eigen::Vector3f::UnitY()) //pitch
+		 	* Eigen::AngleAxisf(0, Eigen::Vector3f::UnitZ()); //yaw
+		 test_pose.orientation.x = q.x();
+		 test_pose.orientation.y = q.y();
+		 test_pose.orientation.z = q.z();
+		 test_pose.orientation.w = q.w();
 
 		poses.push_back(test_pose);
 	}
 
+	// Loop over all options of some variable we want to test performance of
 	// for(int pitch_inc = 5; pitch_inc <= 35; pitch_inc+=5)
 	// {
 	// 	cout <<pitch_inc << endl;
 	// 	if(cand_pts == 6)
-		int cand_pts = 15;
-		for(int i = 0; i < num_poses; i++)
-		{	
-			cout << i << endl;
-			gazebo_msgs::ModelState modelstate;
-			modelstate.model_name = (string) "needle";
-			modelstate.reference_frame = (string) "world";
-			modelstate.pose = poses.at(i);
-			// modelstate.twist = start_twist;
 
-			ros::ServiceClient client = nh.serviceClient<gazebo_msgs::SetModelState>("/gazebo/set_model_state");
-			gazebo_msgs::SetModelState setmodelstate;
-			setmodelstate.request.model_state = modelstate;
-			client.call(setmodelstate);
+	// The number of candidate points to generate
+	// NOTE: Should be equal to max_cand_pts unless you're testing variations
+	// in candidate point numbers
+	int cand_pts = max_cand_pts;
 
-			/** Get Simulation Needle Pose */
-			gazebo_msgs::ModelStates model_states = *(
-				ros::topic::waitForMessage<gazebo_msgs::ModelStates>("/gazebo/model_states", nh)
-			);
-			geometry_msgs::Pose needle_pose = model_states.pose[2];
-			Eigen::Quaternionf q(needle_pose.orientation.w, needle_pose.orientation.x, needle_pose.orientation.y, needle_pose.orientation.z);
-			cv::Point3d loc(needle_pose.position.x, needle_pose.position.y, needle_pose.position.z);
-			NeedlePose true_pose(loc, q);
+	// Run initialization on all poses
+	for (int i = 0; i < num_poses; i++) {
+		cout << i << endl;
 
-			ros::spinOnce();
+		// Update needle pose in Gazebo sim
+		gazebo_msgs::ModelState modelstate;
+		modelstate.model_name = (string)"needle";
+		modelstate.reference_frame = (string)"world";
+		modelstate.pose = poses.at(i);
+		ros::ServiceClient client = nh.serviceClient<gazebo_msgs::SetModelState>("/gazebo/set_model_state");
+		gazebo_msgs::SetModelState setmodelstate;
+		setmodelstate.request.model_state = modelstate;
+		client.call(setmodelstate);
 
-			// cv::namedWindow("l");
-			// cv::imshow("l", l_img);
-			// cv::waitKey(0);
+		// Get Simulation Needle Pose
+		gazebo_msgs::ModelStates model_states = *(
+			ros::topic::waitForMessage<gazebo_msgs::ModelStates>("/gazebo/model_states", nh)
+		);
 
-			// cv::namedWindow("r");
-			// cv::imshow("r", r_img);
-			// cv::waitKey(0);
-
-
-			/** Configure Match Parameters */
-			pfc::match_params params = {
-				0, 360, 25, //yaw
-				0, 360, 25, //pitch
-				0, 360, 25, //roll
-				0.07, 0.18, 0.01, //z
-				cand_pts, // # candidate points to return
-				10, // # points in needle line
-				P_l,
-				P_r
-			};
-
-			/** Create and run initializer */
-			PfcInitializer pfc_init(P_l, P_r, l_img, r_img, params);	
-			pfc_init.run(true, true, true_pose);
-
-			// Write test data to csv file
-			vector<string> results = pfc_init.getResultsAsVector(true_pose);
-			// Add large numbers for all the remaining candidate points not collected for this run
-			int diff = 3*(max_cand_pts - cand_pts);
-			vector<string> addtl(diff, "100000");
-			results.insert(results.end(), addtl.begin(), addtl.end());
-			all_results.push_back(results);
+		geometry_msgs::Pose needle_pose;
+		for(int n =  0; n < model_states.name.size(); n++){
+			if (model_states.name[n] == "needle"){
+				needle_pose = model_states.pose[n];
+			}
 		}
-	// }
-	
-	writeDataListToCSV(all_results);
 
-    return 0;
+		// Convert geometry_msgs::Pose to NeedlePose
+		Eigen::Quaternionf
+			q(needle_pose.orientation.w,
+			  needle_pose.orientation.x,
+			  needle_pose.orientation.y,
+			  needle_pose.orientation.z);
+		cv::Point3d loc(needle_pose.position.x, needle_pose.position.y, needle_pose.position.z);
+		NeedlePose true_pose(loc, q);
+
+		ros::spinOnce();
+
+		/*
+		// Display retrieved images
+		// cv::namedWindow("l");
+		// cv::imshow("l", l_img);
+		// cv::waitKey(0);
+
+		// cv::namedWindow("r");
+		// cv::imshow("r", r_img);
+		// cv::waitKey(0);
+		*/
+
+		/* Configure Match Parameters */
+		pfc::match_params params = {
+			0, 1, 30, //yaw
+			0, 360, 30, //pitch
+			0, 1, 30, //roll
+			0.07, 0.18, 0.01, //z
+			cand_pts, // # candidate points to return
+			10, // # points in needle line
+			P_l,
+			P_r
+		};
+
+		/** Create and run initializer */
+		PfcInitializer pfc_init(P_l, P_r, l_img, r_img, params);
+		pfc_init.run(false, true, true_pose);
+
+		// Write test data to csv file
+		vector<string> results = pfc_init.getResultsAsVector(true_pose);
+
+		// Add arbitrary large number for all the remaining candidate points not collected for this run
+		int diff = 3 * (max_cand_pts - cand_pts);
+		vector<string> addtl(diff, "100000");
+		results.insert(results.end(), addtl.begin(), addtl.end());
+		all_results.push_back(results);
+	}
+	// }
+
+	writeDataListToCSV(all_results, time);
+
+	return 0;
 }
 
